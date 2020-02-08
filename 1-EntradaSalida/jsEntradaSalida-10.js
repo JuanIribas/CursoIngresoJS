d6bcdab01606cb3c99acdf97aa2d402699e893ef
JuanIribas <JuanIribas>
2020-02-07 21:42:40 -0300

    EntradaSalida

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -1,7 +1,7 @@
 /*Debemos lograr tomar el importe por ID.
 Transformarlo a entero (parseInt), luego
 mostrar el importe con un Descuento del 25 %
-en el cuadro de texto "RESULTADO"*/
+en el cuadro de texto "RESULTADO"   ----*/
 function mostrarAumento()
 {
     var importe = document.getElementById("importe").value;
